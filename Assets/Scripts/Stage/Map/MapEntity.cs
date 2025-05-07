8d075b09254ad089b3f4ec9dbc87f07a8d7df5cf
koexe <aicom5448@gmail.com>
2025-05-07 11:21:25 +0900

    Update

--- Assets/Scripts/Stage/Map/MapEntity.cs
+++ Assets/Scripts/Stage/Map/MapEntity.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class MapEntity : MonoBehaviour
 {
     [SerializeField] MapPath[] mapPath;
+    
+    public void MoveMap()
+    {
+
+    }
+
+
 
 
     [System.Serializable]
