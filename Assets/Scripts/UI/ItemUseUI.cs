d5769d700f6f0187535ca1c17ee07a4804643d19
koexe <aicom5448@gmail.com>
2025-06-08 21:42:20 +0900

    Update

---
+++ Assets/Scripts/UI/ItemUseUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemUseUI : UIBase
+{
+    public override void Hide()
+    {
+
+    }
+
+    public override void Initialization(UIData data)
+    {
+
+    }
+
+    public override void Show(UIData _data)
+    {
+
+    }
+}
